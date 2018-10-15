a9a638afd0f3ea048ba3da4bde656944cd091362
dodinhtrung97 <dodinhtrung97@gmail.com>
2018-10-16 06:29:24 +0700

    Added clear canvas button

--- js/app.js
+++ js/app.js
@@ -28,36 +28,10 @@
 	canvas.addEventListener(moveEvent, draw, false);
 	canvas.addEventListener(upEvent, endDraw, false);
 
-	/* PubNub */
-	var channel = 'draw';
-
-	var pubnub = PUBNUB.init({
-		publish_key     : 'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36695',
-		subscribe_key   : 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe',
-		leave_on_unload : true,
-		ssl		: document.location.protocol === "https:"
-	});
-
-	pubnub.subscribe({
-		channel: channel,
-		callback: drawFromStream,
-		presence: function(m){
-			if(m.occupancy > 1){
-				document.getElementById('unit').textContent = 'doodlers';
-			}
-   			document.getElementById('occupancy').textContent = m.occupancy;
-   			var p = document.getElementById('occupancy').parentNode;
-   			p.classList.add('anim');
-   			p.addEventListener('transitionend', function(){p.classList.remove('anim');}, false);
-   		}
-	});
-
-	function publish(data) {
-		pubnub.publish({
-			channel: channel,
-			message: data
-		});
-     }
+	document.getElementById("clear").addEventListener("click", function(e) {
+		event.preventDefault();
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	})
 
     /* Draw on canvas */
     function drawOnCanvas(color, plots) {
@@ -95,7 +69,7 @@
 
     	var x = isTouchSupported ? (e.targetTouches[0].pageX - canvas.offsetLeft) : (e.offsetX || e.layerX - canvas.offsetLeft);
     	var y = isTouchSupported ? (e.targetTouches[0].pageY - canvas.offsetTop) : (e.offsetY || e.layerY - canvas.offsetTop);
-
+    	
     	plots.push({x: (x << 0), y: (y << 0)}); // round numbers for touch screens
 
     	drawOnCanvas(color, plots);
@@ -109,11 +83,7 @@
 	function endDraw(e) {
 	  	e.preventDefault();
 	  	isActive = false;
-	  
-	  	publish({
-	  		color: color,
-	  		plots: plots
-	  	});
+	 
 	  	plots = [];
 	}
 })();
