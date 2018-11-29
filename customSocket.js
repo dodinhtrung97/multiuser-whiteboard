b06b035c9e3662a9c70e60cff6c56ea08acd3fba
dodinhtrung97 <dodinhtrung97@gmail.com>
2018-11-29 18:28:06 +0700

    heroku deploy

--- customSocket.js
+++ customSocket.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const serverUrl = 'http://127.0.0.1:3000'
+const serverUrl = 'https://onlinewhiteboard.herokuapp.com:3000'
 
 const socket = openSocket(serverUrl)
 
