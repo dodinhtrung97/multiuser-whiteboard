c7159416da3ca16f3c608513499885ff72430edc
dodinhtrung97 <dodinhtrung97@gmail.com>
2018-11-29 23:01:55 +0700

    Attempt to connect to server

--- customSocket.js
+++ customSocket.js
@@ -2,7 +2,7 @@ import openSocket from 'socket.io-client';
 
 const port = process.env.PORT || 3000;
 
-const serverUrl = 'https://onlinewhiteboardthesis.herokuapp.com:' + port
+const serverUrl = 'https://whiteboard-server.herokuapp.com:' + 36413
 
 // const serverUrl = 'http://localhost:3000'
 
