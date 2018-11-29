e386318637af1e84a18c093eb5e59b7b6469f783
dodinhtrung97 <dodinhtrung97@gmail.com>
2018-11-29 23:44:28 +0700

    Testing depl

--- customSocket.js
+++ customSocket.js
@@ -2,7 +2,7 @@ import openSocket from 'socket.io-client';
 
 const port = process.env.PORT || 3000;
 
-const serverUrl = 'http://localhost:3000'
+const serverUrl = 'https://whiteboard-server.herokuapp.com'
 
 // const serverUrl = 'http://localhost:3000'
 
